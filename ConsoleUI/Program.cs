82e470f3844bab0ec7d37a1f681d2320c7ddd667
Rıfat Hacıahmetoğlu <rfthao@outlook.com>
2023-05-26 14:32:58 +0300

    26.05.2023

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,7 +16,8 @@ namespace ConsoleUI
 
         private static void ProductDetailTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal(),
+                new CategoryManager(new EfCategoryDal()));
 
             var result = productManager.GetProductDetails();
 
@@ -34,7 +35,7 @@ namespace ConsoleUI
         private static void CategoryTest()
         {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll().Data)
             {
                 Console.WriteLine(category.CategoryName);
             }
@@ -42,7 +43,7 @@ namespace ConsoleUI
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
             foreach (var product in productManager.GetByUnitPrice(40, 100).Data)
             {
                 Console.WriteLine(product.ProductName);
