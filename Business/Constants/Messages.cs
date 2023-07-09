9437c2d2a2821504a96a6dd1229a6a7f98d775dc
Rıfat Hacıahmetoğlu <rfthao@outlook.com>
2023-07-09 16:31:18 +0300

    9.072023

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +14,13 @@ namespace Business.Constants
         public static string ProductsListed = "Ürünler Listelendi";
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
-        internal static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied="Yetkiniz Yok";
+        public static string UserRegistered="Kayıt Başarılı";
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Parola hatalı";
+        public static string SuccessfulLogin="Giriş Başarılı";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token Oluşturuldu.";
     }
 }
